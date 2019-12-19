442df29668363f4e904c61b84d4d29696aaafe0c
fanshys <dmitriy.shab.mail@gmail.com>
2019-12-19 10:47:11 +0500

    10:47 Dima

--- app/js/common.js
+++ app/js/common.js
@@ -1,47 +1,21 @@
-$(function () {
-
-
-	// $(".main-header__slider").owlCarousel({
-	// 	items: 1,
-	// 	nav: true,
-	// 	autoplay: true,
-	// 	autoplayTimeout: 3000,
-	// 	loop: true,
-	// });
-
-	// $(".news-owl").owlCarousel({
-	// 	responsive: {
-	// 		0: {
-	// 			items: 1,
-	// 			slideBy: 1,
-	// 			margin: 30
-	// 		},
-	// 		541: {
-	// 			items: 2,
-	// 			slideBy: 2,
-	// 			margin: 30
-	// 		},
-	// 		1367: {
-	// 			items: 2,
-	// 			slideBy: 2,
-	// 			margin: 40
-	// 		},
-	// 	},
-	// 	nav: true,
-	// 	loop: true,
-	// 	navContainerClass: "news-slider-nav",
-	// 	navClass: ["news-slider-nav__prev", "news-slider-nav__next"],
-	// });
-
-});
 var newsSwiper = new Swiper('.news-slider', {
-	spaceBetween: 40,
 	slidesPerView: 2,
+	spaceBetween: 40,
+	slidesPerGroup: 2,
+	loop: true,
 	navigation: {
 		nextEl: `.news-slider-nav__next`,
 		prevEl: `.news-slider-nav__prev`
-	}
-})
+	},
+	breakpoints: {
+		541: {
+			slidesPerView: 1,
+			spaceBetween: 15,
+			slidesPerGroup: 1
+		}
+	},
+});
+
 var headerSwiper = new Swiper('.main-header__slider', {
 	spaceBetween: 40,
 	slidesPerView: 1
